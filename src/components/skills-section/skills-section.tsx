445969f00eab7637eb2966d0786b9cbff3a559c0
Anton Shcherbinin <shcherbinin.anton.v@gmail.com>
2023-03-12 22:18:43 +0300

    refactor: add image in skills section

--- src/components/skills-section/skills-section.tsx
+++ src/components/skills-section/skills-section.tsx
@@ -1,9 +1,19 @@
+import Image from 'next/image';
+
 export const SkillsSection = () => {
   return (
-    <section className="align-items relative flex h-screen justify-center overflow-x-hidden">
+    <section className="align-items relative flex h-screen justify-center">
       <h1 className="section-heading flex items-center justify-center gap-3 text-center text-4xl font-extrabold sm:text-6xl">
         Work In Progress
       </h1>
+
+      <Image
+        className="image-rendering-pixelated absolute bottom-16 h-40 w-40 sm:h-72 sm:w-72"
+        src="/idea.gif"
+        alt="idea"
+        width={512}
+        height={512}
+      />
     </section>
   );
 };
